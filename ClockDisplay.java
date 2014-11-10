0f1e7e881717c491333c64f96a287e44c31859c8
marce <marce@marce.com>
2014-11-10 16:34:39 +0100

    primer commit -->creacion class ClockDisplay

---
+++ ClockDisplay.java
@@ -0,0 +1,22 @@
+
+/**
+ * Write a description of class ClockDisplay here.
+ * 
+ * @author (your marcelino) 
+ * @version (2014/11/10)
+ */
+public class ClockDisplay
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class ClockDisplay
+     */
+    public ClockDisplay()
+    {
+      
+    }
+
+    
+}
